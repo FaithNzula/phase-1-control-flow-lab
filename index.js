a23b58d709ab8c56628be4c6b749352432851235
Faith Nzula Matei <faithnzula1230@gmail.com>
2022-08-18 20:24:27 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet) {
+  if (feet <= 500) {
+    return `This one is on me!`
+  }
+  else if (feet > 2000) {
+    return `I will gladly take your thirty bucks.`
+  }
+  else (feet >= 2500); {
+    return `No can do.`
+  };
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
+  if (city === "NYC"){
+    return `Ok, sounds good.`
+  }
+  else {
+    return `No go.`
+  }
+  
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return `Thank you so much.`
+      break;
+    case 'not as generous':
+      return `Thank you.`
+      break;
+      default:
+      return `Bye.`
+  }
 }
